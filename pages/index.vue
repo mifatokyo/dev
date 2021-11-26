68794d7a8fe54a68088c15be3eb4f781a40f4f68
imai <imai@mifa.tokyo>
2021-11-26 12:45:36 +0900

    index

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,62 @@
       </div>
       <Topics/>
     </section>
+    <div class="z-0 relative front-bg">
+      <div class="float _grd"></div>
+      <section class="front-works inner section">
+        <div class="">
+          <h2 class="front-headline2 md:text-7xl text-5xl leading-loose" data-text="Works">WORKS</h2>
+        </div>
+        <Works/>
+        <nuxt-link to="/works" class="button trans">
+          <span>VIEW ALL</span><em></em>
+        </nuxt-link>
+      </section>
+    </div>
+    <section class="front-service inner section">
+      <div class="float _org"></div>
+      <div class="">
+        <h2 class="front-headline2 md:text-7xl text-5xl leading-loose" data-text="Concept">CONCEPT</h2>
+      </div>
+      <Service/>
+      <nuxt-link to="/service" class="button trans">
+        <span>SERVICE</span><em></em>
+      </nuxt-link>
+    </section>
+    <!-- <div class="z-0 relative front-bg">
+      <section class="front-blog inner section">
+        <div class="">
+          <h2 class="front-headline2  md:text-7xl text-5xl leading-loose" data-text="Blog">BLOG</h2>
+        </div>
+        <ul class="blog-list grid md:gap-5 gap-10 grid-cols-1 md:grid-cols-3 p-4">
+          <li v-for="content in contents" :key="content.id" class="blog-item">
+            <nuxt-link :to="`/blog/${content.id}`">
+              <div class="post-image shadow-md">
+                <span class="text-white text-xs font-semibold category sample">{{ content.category.name }}</span>
+                <picture>
+                  <source :srcset="content.thumbnail.url + '?fm=webp'" type="image/webp">
+                  <img :src="content.thumbnail.url">
+                </picture>
+              </div>
+              <div class="post-text">
+                <h3 class="title">{{ content.title }}</h3>
+                <time class="text-gray-400 md:text-sm text-xs font-semibold time">{{ new Date(content.datepick).toLocaleDateString() }}</time>
+              </div>
+            </nuxt-link>
+          </li>
+        </ul>
+        <nuxt-link to="/blog" class="button trans">
+          <span>VIEW ALL</span><em></em>
+        </nuxt-link>
+      </section>
+    </div>
+    <section id="contact" class="inner section">
+      <div class="float _red"></div>
+      <div class="">
+        <h2 class="front-headline2 md:text-7xl text-5xl leading-loose" data-text="Contact">CONTACT</h2>
+      </div>
+      <Contact/>
+    </section> -->
     <Footer/>
   </div>
 </template>
@@ -18,7 +74,7 @@
   import Footer from "@/components/SiteFooter.vue";
   import MainView from "@/components/MainView.vue";
   import Topics from "@/components/FrontTopics.vue";
-  // import Service from "@/components/FrontService.vue";
+  import Service from "@/components/FrontService.vue";
   // import Works from '@/components/Works.vue';
   // import Contact from '@/components/Contact.vue';
 
@@ -29,7 +85,7 @@
       MainView,
       Topics,
       // Works,
-      // Service,
+      Service,
       // Contact,
 		},
 
